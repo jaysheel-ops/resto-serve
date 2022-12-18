1e9fe2055553a526579ad48323eee734f3f8f663
Jaysheel Dodia <73154873+jaysheel-ops@users.noreply.github.com>
2022-12-19 00:48:49 +0530

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const supaFunc = require('./controller/index');
 
 const app =   express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const root = {root: "."}
 
 app.use(bodyParser.json());
